fccd9d2d6da7dd7ce21e237a2e6bc52c5b6bc100
Ali Salama <alisalamadev@gmail.com>
2025-08-06 05:36:10 +0300

    inital a test api

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express'
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello')
+})
+
+app.get('/about', (req: Request, res: Response) => {
+    res.send('about')
+})
+
+app.listen(port, () => {
+    console.log(`this port is listening in ${port}`)
+})
